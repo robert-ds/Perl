a6515d206389d86856c0a0d382a86ef55d22c106
Robert Vasquez <vasquez.robert.andres@gmail.com>
2019-07-15 16:11:46 -0400

    Cadenas Multilineas

---
+++ cadenasMultilineas.pl
@@ -0,0 +1,8 @@
+#!/usr/bin/perl
+
+#Para Imprimir Cadenas con Muliples Lineas se Recomienda Utilizar Comillas Simples
+$cadena ='Esto es
+un Mensaje
+de Varias Lineas';
+
+print "Mensaje: $cadena\n";
