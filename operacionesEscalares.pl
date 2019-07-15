1dc355843a1113cd1247b03ada8dbca3faae03ac
Robert Vasquez <vasquez.robert.andres@gmail.com>
2019-07-15 16:07:58 -0400

    Operaciones Basicas Con Variables Escalares

---
+++ operacionesEscalares.pl
@@ -0,0 +1,14 @@
+#!/usr/bin/perl
+
+#Operaciones Escalares
+
+$str = "Robert" . "Andres";
+$sum = 5 + 10;
+$mul = 4 * 5;
+$mix = $str . $sum;
+
+print ".:Operaciones Con Escalares:.\n";
+print "STR = $str\n";
+print "SUM = $sum\n";
+print "MUL = $mul\n";
+print "MIX = $mix\n";
