86bde72c1e6f6f1e65583d5ea10567a94cbf53f8
Robert Vasquez <vasquez.robert.andres@gmail.com>
2019-07-17 16:32:44 -0400

    Función SORT Para Ordenar Matrices

---
+++ ordenandoMatriz.pl
@@ -0,0 +1,10 @@
+#!/usr/bin/perl
+
+#Ordenando Matrices, Función Sort
+
+@frutas = qw(Mango Cambur Platano Papaya Fresa);
+print("Antes: @frutas\n");
+
+@frutas = sort(@frutas);
+print("Despues: @frutas\n");
+
