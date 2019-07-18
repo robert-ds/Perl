084fb0db88814dc6cce1f5c0784f2ceabb177916
Robert Vasquez <vasquez.robert.andres@gmail.com>
2019-07-18 15:27:53 -0400

    Sentencia ifElse 'Si No'

---
+++ Estructuras_de_Control/ifElse.pl
@@ -0,0 +1,23 @@
+#!/usr/bin/perl
+
+#Estructura if Else
+
+$a = 100;
+
+if($a < 20){
+	printf "La Variables a es Menor que 20\n";
+}else{
+	printf "La Variable a es Mayor a 20\n";
+}
+
+print "El Valor de a es: $a\n";
+
+
+$a = "";
+
+if($a){
+ 	printf "La Variable a No Esta Vacia!\n";
+}else{
+	printf "La Variable a Esta Vacia!\n";
+}
+print "El Valor de la Variable a es: $a\n";
