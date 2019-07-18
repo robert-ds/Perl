1001af0ff58bdf9f50cee762bce831e2f21188c1
Robert Vasquez <vasquez.robert.andres@gmail.com>
2019-07-18 15:39:39 -0400

    La Sentencia unless 'A menos que'

---
+++ Estructuras_de_Control/unLess.pl
@@ -0,0 +1,17 @@
+#!/usr/bin/perl
+
+#UNLESS 'A Menos'
+
+$a = 20;
+
+unless($a < 15){
+	printf "15 No es Mayor que a.\n";
+}
+print "El Valor de a es: $a\n";
+
+$a = "";
+
+unless($a){
+	printf "El Valor de a Esta Vacio!.\n";
+}
+print "Valor de a: $a\n";
