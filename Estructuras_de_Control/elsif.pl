4e75b88960f59cacb9ec3add2deabd5ae5eb1e73
Robert Vasquez <vasquez.robert.andres@gmail.com>
2019-07-18 15:29:09 -0400

    Sentencia ELSIF 'Si No Si'

---
+++ Estructuras_de_Control/elsif.pl
@@ -0,0 +1,13 @@
+#!/usr/bin/perl
+
+#Sentencia if ElseIF
+
+$a = 100;
+
+if($a == 20){
+	printf "El Valor de a es 20.\n";
+}elsif($a == 30){
+	printf "El Valor de a es 30.\n";
+}else{
+	printf "El Valor de a es: $a\n";
+}
