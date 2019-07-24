a0033cc309a44ca3ed7f20dbdce51adde23800b5
Robert Vasquez <vasquez.robert.andres@gmail.com>
2019-07-24 10:13:05 -0400

    Ciclo doWHILE, Ejecuta las instrucciones almenos una Vez

---
+++ Ciclos/doWhile.pl
@@ -0,0 +1,10 @@
+#!/usr/bin/perl
+
+#Ciclo do While
+#Este ciclo garantiza que las instrucciones se ejecuten almenos una vez
+
+$n = 1000;
+
+do{
+	printf("Imprime \$n = $n Solo Una Vez porque la Condicion Establecida es Falsa!\n");
+}while($n == 0);
