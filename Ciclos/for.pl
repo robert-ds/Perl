ef65b28b5219a5ba9bc2161238fda7a64a0ebbbc
Robert Vasquez <vasquez.robert.andres@gmail.com>
2019-07-24 09:55:56 -0400

    Ciclo For

---
+++ Ciclos/for.pl
@@ -0,0 +1,9 @@
+#!/usr/bin/perl
+
+#Ciclo For,
+
+#Tanto la inicialización de la Variable, como la condición y el incremento se declaran dentro del
+#Ciclo
+for($n = 100;$n <= 1000; $n = $n + 100){
+		printf("Aumenta: $n\n");
+}
