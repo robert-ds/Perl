bdd1c4f8cd94a18d8d26ed3ac515f75419e4543b
Robert Vasquez <vasquez.robert.andres@gmail.com>
2019-07-24 09:42:04 -0400

    Ciclo WHILE, Mientras la Condición se Cumpla el Ciclo se Repetira

---
+++ Ciclos/while.pl
@@ -0,0 +1,12 @@
+#!/usr/bin/perl
+
+#Bucle WHILE, Mientras se Cumpla la Condicion el ciclo o instrucciones se Repetira
+
+#Inicializamos la Variable
+$n = 1;
+
+#Estructura Basica
+while($n < 10){
+	printf("Aumenta: $n\n");
+	$n = $n + 1; 				#Determina el Incremento de la variable, en este caso de 1 en 1.
+}
