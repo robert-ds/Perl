d66326a09d7d177a14507cf61220dc0db285db21
Robert Vasquez <vasquez.robert.andres@gmail.com>
2019-07-29 11:18:15 -0400

    Diferentes Formas de Trabajar con la Función open()

--- Archivos_I_O/abrirArchivos.pl
+++ Archivos_I_O/abrirArchivos.pl
@@ -1,2 +1,20 @@
 #!/usr/bin/perl
 
+#Acceso de Solo Lectura
+#open(DATA, "<text.txt") or die "Could't open file text.txt, $!";
+
+#Trunca (Vacía) el Archivo
+#open(DATA, ">text.txt") or die "Could't open file text.txt, $!";
+
+#Abre un Archivo, Truncandolo Primero
+open(DATA, "+>text.txt") or die "Could't open file text.txt, $!";
+
+#Actualizar el Archivo Sin truncarlo
+#open(DATA, "+<text.txt") or die "Could't open file text.txt, $!";
+
+#Abrir Archivo en Modo de Adicción, el punto de Escritura se Establece al final
+#open(DATA, ">>text.txt") || or die "Could't open file text.txt, $!";
+
+#Para Abjuntar al final del Archivo
+#open(DATA, "+>>text.txt") || or die "Could't open file text.txt, $!";
+
