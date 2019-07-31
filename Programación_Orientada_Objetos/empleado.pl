5ee36cc93d9c8615108db31aeaad9508c3049693
Robert Vasquez <vasquez.robert.andres@gmail.com>
2019-07-31 16:47:42 -0400

    Uso e Instanciación de la Clase Persona

---
+++ Programación_Orientada_Objetos/empleado.pl
@@ -0,0 +1,16 @@
+#!/usr/bin/perl -w
+
+use clasePersona;
+
+$objeto = new Persona("Robert","Vasquez",84395823523);
+#Obtener Primer Nombre Donde se Establecio Usando el Constructor
+$primerNombre = $objeto->obtenerPrimerNombre();
+print "Antes de Establecer el nombre es: $primerNombre\n";
+
+#Ahora Establecemos el Nombre utilizando el método establecerPrimerNombre()
+$objeto->establecerPrimerNombre("Andres");
+
+#Obteniendo el Nuevo Nombre
+$primerNombre = $objeto->obtenerPrimerNombre();
+print "Nuevo Nombre Establecido: $primerNombre\n";
+
